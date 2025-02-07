f1ea532bb6f39f6df7cff9d3c6c4dbe5866a73db
Alessandro Araujo <alessandro.v.b.araujo@gmail.com>
2025-02-07 14:48:45 -0300

    creating routes and tasks

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Rotas de Login
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('login.form');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/create-user-login', [LoginController::class, 'showCreateForm'])->name('login.create');
+Route::post('/create-user', [LoginController::class, 'create'])->name('login.store');
+
+// Rotas Protegidas por Middleware de Autenticação
+Route::middleware(['auth'])->group(function() {
+    // Rotas de Usuários (CRUD)
+    Route::resource('users', UserController::class)->except(['index', 'show']);
+
+    // Rotas de View de Usuários
+    Route::get('/users', [UserController::class, 'index'])->name('user.index');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('user.show');
+    Route::get('/users/create', [UserController::class, 'create'])->name('user.create');
+    Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 });
